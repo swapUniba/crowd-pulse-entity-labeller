7653bb4aa6f2945a7e53e291258ba097006903fe
vitoturing <cuccovito@gmail.com>
2017-10-16 22:24:46 +0200

    Initial Commit

--- src/main/java/MessageLabellerPlugin.java
+++ src/main/java/MessageLabellerPlugin.java
@@ -1,6 +1,8 @@
 import com.github.frapontillo.pulse.crowd.data.entity.Message;
 import com.github.frapontillo.pulse.spi.IPlugin;
 import rx.Observable;
+import rx.Subscriber;
+import rx.observers.SafeSubscriber;
 
 public class MessageLabellerPlugin extends IPlugin<Message,Message,MessageLabellerConfig> {
 
@@ -11,11 +13,27 @@ public class MessageLabellerPlugin extends IPlugin<Message,Message,MessageLabell
 
     @Override
     public MessageLabellerConfig getNewParameter() {
-        return null;
+        return new MessageLabellerConfig();
     }
 
     @Override
     protected Observable.Operator<Message, Message> getOperator(MessageLabellerConfig messageLabellerConfig) {
-        return null;
+        return subscriber -> new SafeSubscriber<>(new Subscriber<Message>() {
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Message message) {
+
+            }
+        });
     }
 }
